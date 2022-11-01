9f197a00cb04af25621d5b742af3b94dbb483ea2
Oleg Golibroda <oleg.golibroda@iway.ch>
2022-11-01 08:42:38 +0100

    better?

--- django_wishlist/settings.py
+++ django_wishlist/settings.py
@@ -18,7 +18,7 @@ from django.core.management.utils import get_random_secret_key
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
